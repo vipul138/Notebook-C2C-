e67903b4711bd275726d9956c63317ba3e32b0bc
vipul <vipulvishwakarma111@gmail.com>
2022-03-31 14:08:23 +0530

    db change to psql for deply

--- app.py
+++ app.py
@@ -10,19 +10,26 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 from PIL import Image
 import secrets
 import os, re
+from flask_mail import Message, Mail
+from itsdangerous import Serializer
+
+# mysql://root:root@localhost/notebook
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/notebook'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://pxycfplptqiebe:5bc7f7949aeddff4bb821fb9defdc5ca1b624edbd153a6bec5380bcd9023e300@ec2-54-157-79-121.compute-1.amazonaws.com:5432/dann5tncoaa5dp'
 app.config['SECRET_KEY'] = 'secret'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-
-
 login_manager = LoginManager(app)
 login_manager.init_app(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = 'info'
-
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'vipulvishwakarma786@gmail.com'
+app.config['MAIL_PASSWORD'] = 'vipulserver786'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+mail = Mail(app)
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -37,6 +44,19 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(200), nullable=False)
     books = db.relationship('Books_list', backref='owner', lazy=True)
 
+    def get_reset_token(self):
+        s = Serializer(app.config['SECRET_KEY'])
+        return s.dumps({'user_id': self.id})
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
     def __repr__(self):
         return f"User('{self.id}', '{self.username}', '{self.email}')"
 
@@ -44,7 +64,7 @@ class User(db.Model, UserMixin):
 class Books_list(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     book_name = db.Column(db.String(80), nullable=False)
-    book_img = db.Column(db.String(200), nullable=False, default='book.jpg')
+    book_img = db.Column(db.String(200), nullable=False, default='book.png')
     semester = db.Column(db.Integer, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     price = db.Column(db.Integer, nullable=False)
@@ -54,11 +74,8 @@ class Books_list(db.Model):
     def __repr__(self):
         return f"Books_list('{self.book_name}', '{self.book_img}', '{self.date_posted}')"
 
-# db.drop_all()
-# db.create_all()
 
 class RegistrationForm(FlaskForm):
-    # id = IntegerField(validators=[DataRequired()])
     username = StringField(validators=[DataRequired(), Length(min=2, max=30)])
     email = StringField(validators=[DataRequired(), Email()])
     password = PasswordField(validators=[DataRequired()])
@@ -66,9 +83,14 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
+        pattern = re.compile("\w")
+        if pattern.match(username.data):
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+        else:
+            raise ValidationError("enter correct number")
+
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
@@ -103,7 +125,6 @@ class UpdateAccountForm(FlaskForm):
 
 class UploadBookForm(FlaskForm):
     bookname = StringField(validators=[DataRequired()])
-    # bookimg =  FileField('upload img',validators=[FileAllowed(['jpg', 'png'])])
     semester = SelectField(choices = [('1', '1 Sem'), ('2', '2 Sem'), ('3', '3 Sem'), 
                                         ('4', '4 Sem'), ('5', '5 Sem'), ('6', '6 Sem')])
     price = IntegerField(validators=[DataRequired()])
@@ -116,6 +137,23 @@ class UploadBookForm(FlaskForm):
             pass
         else:
             raise ValidationError("enter correct number")
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
 
 
 @app.route('/')
@@ -132,7 +170,7 @@ def home():
 
 @app.route('/login', methods=['Get', 'Post'])
 def login():
-    form=LoginForm()
+    form = LoginForm()
 
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -157,7 +195,7 @@ def register():
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
+        flash('Your account is created!', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
@@ -169,31 +207,18 @@ def logout():
     return redirect(url_for('logo'))
 
 
-def save_pic(form_picture):
-    random_hex = secrets.token_hex(8)   # generates hexadecimal no of image
-    _, f_ext = os.path.splitext(form_picture.filename)   # this returns 2 value name and extension of file
-    picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path,'static/profile_pic', picture_fn) # saving image on a path
+def change_pic(update_picture):
+    hex = secrets.token_hex(8)   # generates hexadecimal no of image
+    _, extension = os.path.splitext(update_picture.filename)   # this returns 2 value name and extension of file
+    picture_fn = hex + extension
+    path = os.path.join(app.root_path, 'static/profile_pic', picture_fn) # saving image on a path
 
-    output_size = (125,125)          # resize the large image to small
-    i = Image.open(form_picture)
+    output_size = (125, 125)          # resize the large image to small
+    i = Image.open(update_picture)
     i.thumbnail(output_size)
-    i.save(picture_path)
+    i.save(path)
 
     return picture_fn
-
-# def save_book(form_bookimg):
-#     random_hex = secrets.token_hex(8)   # generates hexadecimal no of image
-#     _, f_ext = os.path.splitext(form_bookimg.filename)   # this returns 2 value name and extension of file
-#     bookimg_fn = random_hex + f_ext
-#     bookimg_path = os.path.join(app.root_path,'static/image', bookimg_fn) # saving image on a path
-    
-#     output_size = (125,125)          # resize the large image to small 
-#     i = Image.open(form_bookimg)
-#     i.thumbnail(output_size)
-#     i.save(bookimg_path)
-
-#     return bookimg_fn 
 
 
 @app.route('/account', methods=['Get', 'Post'])
@@ -202,7 +227,7 @@ def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
         if form.picture.data:
-            picture_file = save_pic(form.picture.data)
+            picture_file = change_pic(form.picture.data)
             current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
@@ -247,21 +272,20 @@ def update_book(user_id):
         abort(403)
     form = UploadBookForm()
     if form.validate_on_submit():
-        book.book_name=form.bookname.data
-        book.semester=form.semester.data
-        book.price=form.price.data
-        book.phone=form.phone.data
+        book.book_name = form.bookname.data
+        book.semester = form.semester.data
+        book.price = form.price.data
+        book.phone = form.phone.data
         db.session.commit()
         flash('book updation has been done', 'success')
         return redirect(url_for('home', user_id=book.id))
 
     elif request.method == 'GET':
-        form.bookname.data=book.book_name
-        form.semester.data=book.semester
-        form.price.data=book.price
-        form.phone.data=book.phone
+        form.bookname.data = book.book_name
+        form.semester.data = book.semester
+        form.price.data = book.price
+        form.phone.data = book.phone
     return render_template('sellbook.html', title='update book', form=form, legend='Update Book')
-
 
 
 @app.route('/book/<int:user_id>/delete', methods=['Get', 'Post'] )
@@ -284,6 +308,47 @@ def user_book(username):
                 .order_by(Books_list.date_posted.desc())\
                 .paginate(page=page, per_page=4)
     return render_template('user_book.html', title='userBook',books=books, user=user)
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                sender='noreply@gmail.com',
+                  recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('reset_password', token=token, _external=True)}
+Ignore this message if request is not made by you.
+'''
+    mail.send(msg)
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('email has been sent to registered email_id', 'info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', title='Reset Password', form=form)
+
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('This link has been expired', 'warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been updated!', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', title='Reset Password', form=form)
 
 
 if __name__ == '__main__':
