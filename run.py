2384ffd67c2aef5bbb2548f9831c64a214aa72e7
vipul <vipulvishwakarma111@gmail.com>
2022-04-01 12:46:08 +0530

    new deploy

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-from project import app
+from project import app, db
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    # app.run(debug=True, port=8000)
+    app.run(host='0.0.0.0', port=8080)
+db.execute
