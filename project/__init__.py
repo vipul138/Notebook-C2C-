405b6f729bf7ec50a755c01ba556618d817d64cd
vipul138 <64353004+vipul138@users.noreply.github.com>
2022-04-19 07:55:54 -0700

    comment

--- project/__init__.py
+++ project/__init__.py
@@ -8,7 +8,9 @@ from flask_mail import Mail
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tparlisdlxurdz:ea35bd90e87172065f3f49525eadb16fda5808372caf8db50987ad1ebaddf81e@ec2-3-230-122-20.compute-1.amazonaws.com:5432/d5i3qv074m1dmh'
 app.config['SECRET_KEY'] = 'secret'
-login_manager = LoginManager(app)
+login_manager = LoginManager(app) # login manager contains the code that lets your application and Flask-Login work together, 
+                                    # such as how to load a user from an ID, 
+                                    # where to send users when they need to log in
 login_manager.init_app(app)
 login_manager.login_view = "login"                    # if user tries to route homepage w/o logging in it redirect user to login page
 login_manager.login_message_category = 'info'         # setting message category
